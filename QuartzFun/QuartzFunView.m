2cf362e5506670aecb1c91fe1ea97b25bdb53dd0
Burenkovuser <nb2207@yandex.ru>
2017-07-01 09:20:50 +0700

    draw rect and draw ellipse

--- QuartzFun/QuartzFunView.m
+++ QuartzFun/QuartzFunView.m
@@ -51,15 +51,33 @@
     CGContextSetLineWidth(context, 2.0);//линия толщеной 2
     CGContextSetStrokeColorWithColor(context, _currentColor.CGColor);//цвет
     
+    CGContextSetFillColorWithColor(context, _currentColor.CGColor);//заливка для элипса и прямоугольника
+    //координаты и размеры (высота и ширина)
+    CGRect currentRect = CGRectMake(_firstTouch.x,
+                                    _firstTouch.y,
+                                    _lastTouch.x - _firstTouch.x,
+                                    _lastTouch.y - _firstTouch.y);
+    
     switch (_shapeType) {
             case kLineShape://линия
             CGContextMoveToPoint(context, _firstTouch.x, _firstTouch.y);
             CGContextAddLineToPoint(context, _lastTouch.x, _lastTouch.y);
             CGContextStrokePath(context);
             break;
-            case kRectShape: break;
-            case kEllipseShape: break;
-            case kImageShape: break;
+            
+            case kRectShape://квадрат
+            CGContextAddRect(context, currentRect);
+            CGContextDrawPath(context, kCGPathFillStroke);
+            break;
+            
+            case kEllipseShape://элипс
+            CGContextAddEllipseInRect(context, currentRect);
+            CGContextDrawPath(context, kCGPathFillStroke);
+            break;
+            
+            case kImageShape:
+            break;
+            
         default:
         break;
     }
